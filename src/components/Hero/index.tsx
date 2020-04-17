b5387f9c2d37ace4fce697fb93859696ee0102d0
Giovanna Matheus <gigimatheus16@gmail.com>
2020-04-16 22:17:08 -0300

    direções do personagem

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,20 +1,48 @@
+import useEventListener from '@use-it/event-listener';
 import React from 'react';
 import { TILE_SIZE } from '../settings/constants';
 
 import './index.css';
 
+const initialPosition = {
+    x: 15,
+    y: 15
+};
+
 const Hero = () => {
+    const [positionState, updatePositionState] = React.useState(initialPosition);
+    const [direction, updateDirectionState] = React.useState('RIGHT');
+
+    useEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+            updatePositionState({ x: positionState.x - 1, y:positionState.y });
+            updateDirectionState('LEFT');
+
+        } else if (event.key === 'ArrowRight') {
+            updatePositionState({ x: positionState.x + 1, y:positionState.y });
+            updateDirectionState('RIGHT');
+            
+        } else if (event.key === 'ArrowDown') {
+            updatePositionState({ x: positionState.x, y:positionState.y -1 });
+        
+        } else if (event.key === 'ArrowUp') {
+            updatePositionState({ x: positionState.x, y:positionState.y +1 });
+        }
+    })
+
     return (
         <div
             style={{
+                position: 'absolute',
+                bottom: TILE_SIZE * positionState.y,
+                left: TILE_SIZE * positionState.x, 
                 width: TILE_SIZE,
                 height: 100,
                 backgroundImage: "url(./assets/HERO.png)",
                 backgroundRepeat: 'no-repeat',
+                backgroundPosition: `0 ${TILE_SIZE}`,
                 animation: 'hero-animation 1s steps(4) infinite',
-                position: 'absolute',
-                bottom: 48 * 2,
-                left: 48 * 1, 
+                transform: `scaleX(${direction === 'RIGHT' ? 1 : -1})`
             }} 
         />
     );
